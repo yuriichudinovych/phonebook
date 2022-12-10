4b920d5e36aa96300edd0d70c551e54dadbdbfff
yuriichudinovych <87262619+yuriichudinovych@users.noreply.github.com>
2022-12-10 14:55:20 +0000

    Deploying to gh-pages from @ yuriichudinovych/phonebook@103abe950ac32e1469bfa4cee9f482cc0bba600a 🚀

---
+++ static/js/794.8aaa9b24.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,a){a.r(t);var c=a(7689),r=a(184);t.default=function(){return(0,r.jsx)(c.Fg,{to:"/contacts"})}}}]);
+//# sourceMappingURL=794.8aaa9b24.chunk.js.map
